4f94902c2c580ecc169b48564f10d71562c12f74
Durgesh <84910181+Durgesh-babu@users.noreply.github.com>
2024-01-24 20:09:49 +0530

    fix: name arg bug

--- src/components/Player.js
+++ src/components/Player.js
@@ -3,7 +3,7 @@ import Gameboard from './Gameboard';
 export default class Player {
   constructor(name, turn = false) {
     this.name = name;
-    this.gameboard = new Gameboard(this.name);
+    this.gameboard = new Gameboard();
     this.turn = turn;
   }
 }
