ea33325462c712c6dff2e9ad8fc590c56ab7598d
Durgesh <84910181+Durgesh-babu@users.noreply.github.com>
2024-01-25 14:17:25 +0530

    add: manual ship placement & create players

--- src/index.js
+++ src/index.js
@@ -1,3 +1,24 @@
-export default function Test() {
-  return 'Testing';
-}
+import './style.css';
+import Player from './components/Player';
+import Computer from './components/Computer';
+
+const human = new Player('Human', true);
+const computer = new Computer();
+
+human.gameboard.placeShip(1, 2);
+human.gameboard.placeShip(1, 4);
+human.gameboard.placeShip(2, 6);
+human.gameboard.placeShip(6, 3);
+human.gameboard.placeShip(6, 7);
+human.gameboard.placeShip(4, 8);
+human.gameboard.placeShip(8, 1);
+
+computer.gameboard.placeShip(0, 2);
+computer.gameboard.placeShip(1, 4);
+computer.gameboard.placeShip(2, 6);
+computer.gameboard.placeShip(6, 3);
+computer.gameboard.placeShip(6, 7);
+computer.gameboard.placeShip(4, 8);
+computer.gameboard.placeShip(8, 1);
+
+
