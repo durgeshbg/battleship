beddd69801d41c51503a6170a7239f222f210fca
Durgesh <84910181+Durgesh-babu@users.noreply.github.com>
2024-01-25 19:35:26 +0530

    update: move boardListener to placeShips (imported)

--- src/index.js
+++ src/index.js
@@ -2,26 +2,13 @@ import './style.css';
 import Player from './components/Player';
 import Computer from './components/Computer';
 import populateBoard from './DOM/populateBoard';
-import boardListener from './DOM/boardListener';
+import placeShips from './DOM/placeShips';
 
-const human = new Player('Human', true);
-const computer = new Computer();
+export default function init() {
+  const human = new Player('Human', true);
+  const computer = new Computer();
 
-human.gameboard.placeShip(1, 2);
-human.gameboard.placeShip(1, 4);
-human.gameboard.placeShip(2, 6);
-human.gameboard.placeShip(6, 3);
-human.gameboard.placeShip(6, 7);
-human.gameboard.placeShip(4, 8);
-human.gameboard.placeShip(8, 1);
-
-computer.gameboard.placeShip(0, 2);
-computer.gameboard.placeShip(1, 4);
-computer.gameboard.placeShip(2, 6);
-computer.gameboard.placeShip(6, 3);
-computer.gameboard.placeShip(6, 7);
-computer.gameboard.placeShip(4, 8);
-computer.gameboard.placeShip(8, 1);
-
-populateBoard(human, computer);
-boardListener(human, computer);
+  populateBoard(human, computer);
+  placeShips(human, computer);
+}
+init();
