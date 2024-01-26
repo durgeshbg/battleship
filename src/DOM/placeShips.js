7e29344cd663cf4ad99c137134eca45fa08c4839
Durgesh <84910181+Durgesh-babu@users.noreply.github.com>
2024-01-26 23:45:00 +0530

    add: placeRandom for leftPlayer function

--- src/DOM/placeShips.js
+++ src/DOM/placeShips.js
@@ -32,6 +32,13 @@ export default function placeShips(leftPlayer, rightPlayer) {
   placeRandom(rightPlayer);
   populateBoard(leftPlayer, rightPlayer);
 
+  document.querySelector('.random .option').onclick = () => {
+    document.querySelector('.restart .option').click();
+    placeRandom(leftPlayer);
+    populateBoard(leftPlayer, rightPlayer);
+    boardListener(leftPlayer, rightPlayer);
+  };
+
   placeMannual(
     leftPlayer,
     document.querySelector('.gameboard .left'),
