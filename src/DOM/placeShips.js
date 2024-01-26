5588b4fff3248633b4abd1edc2971291269c0ba8
Durgesh <84910181+Durgesh-babu@users.noreply.github.com>
2024-01-26 23:30:18 +0530

    feat: ship orientation while placeMannual

--- src/DOM/placeShips.js
+++ src/DOM/placeShips.js
@@ -6,19 +6,23 @@ function placeRandom(player) {
   while (player.gameboard.ships.length) {
     const x = Math.floor(Math.random() * 10);
     const y = Math.floor(Math.random() * 10);
-    player.gameboard.placeShip(x, y);
+    const z = Math.floor(Math.random() * 2);
+    player.gameboard.placeShip(x, y, z);
   }
 }
 
-function placeMannual(player, board, opponent) {
+function placeMannual(player, board, opponent, z = 0) {
+  document.querySelector('.rotate .option').onclick = () => {
+    z = z === 0 ? 1 : 0;
+  };
   board.childNodes.forEach((row, i) => {
     row.childNodes.forEach((cell, j) => {
       cell.onclick = () => {
-        player.gameboard.placeShip(i, j);
+        player.gameboard.placeShip(i, j, z);
         populateBoard(player, opponent);
         if (player.gameboard.ships.length === 0)
           boardListener(player, opponent);
-        else placeMannual(player, board, opponent);
+        else placeMannual(player, board, opponent, z);
       };
     });
   });
